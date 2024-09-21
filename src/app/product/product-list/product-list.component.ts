553cb33a1a0cf6b3a8748e840f6fc66a24921d8c
Alexis Miguel Hernandez Ovalle <miguelovalle2909@gmail.com>
2024-09-20 20:52:38 -0600

    "Commit viernes 20 de septiembre"

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -7,36 +7,37 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent {
 
+
   productos:any[]=[
     {
       "productoId":1,
       "Modelo":"Sentra",
       "Descripcion":"4 puertas",
-      "Precio": 20000,
+      "Precio": 350000,
       "Year":2023,
       "Marca":"NISSAN",
       "Color":"Azul",
-      "ImagenUrl":"" 
+      "ImagenUrl":"https://acroadtrip.blob.core.windows.net/catalogo-imagenes/xl/RT_V_0c7166c16ff74073b65851f7f0b360ed.jpg" 
     },
     {
       "productoId":2,
       "Modelo":"A4",
       "Descripcion":"4 puertas",
-      "Precio": 30000,
+      "Precio": 510000,
       "Year":2022,
       "Marca":"AUDI",
       "Color":"Blanco",
-      "ImagenUrl":"" 
+      "ImagenUrl":"https://acroadtrip.blob.core.windows.net/catalogo-imagenes/s/RT_V_7dddd9bd89814f5e9cbbbafdc08915ab.jpg" 
     },
     {
       "productoId":3,
-      "Modelo":"Rio",
+      "Modelo":"Cheyenne",
       "Descripcion":"4 puertas",
-      "Precio": 15000,
-      "Year":2020,
-      "Marca":"KIA",
+      "Precio": 970000,
+      "Year":2024,
+      "Marca":"CHEVROLET",
       "Color":"Azul",
-      "ImagenUrl":"" 
+      "ImagenUrl":"https://www.chevrolet.com.mx/content/dam/chevrolet/na/mx/es/index/pickups-and-trucks/2024-cheyenne/mov/01-images/cheyenne-zr2-v2.jpg?imwidth=960" 
     }
 
   ]
