8d3b9d776280b3857f9704e9fa6774a217889b81
Alexis Miguel Hernandez Ovalle <miguelovalle2909@gmail.com>
2024-09-21 10:12:09 -0600

    "Sabado 21 formulario"

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProductos } from '../productos';
 
 @Component({
   selector: 'app-product-list',
@@ -8,13 +9,22 @@ import { Component } from '@angular/core';
 export class ProductListComponent {
 
 
-  productos:any[]=[
+    imageWith:number=60;
+    imageMargin:number=2;
+    muestraImg:boolean=true;
+    listFilter:string=''
+
+    muestImg():void{
+      this.muestraImg=!this.muestraImg;
+    }
+
+  productos:IProductos[]=[
     {
       "productoId":1,
       "Modelo":"Sentra",
       "Descripcion":"4 puertas",
       "Precio": 350000,
-      "Year":2023,
+      "Year":"febrero 4 2023",
       "Marca":"NISSAN",
       "Color":"Azul",
       "ImagenUrl":"https://acroadtrip.blob.core.windows.net/catalogo-imagenes/xl/RT_V_0c7166c16ff74073b65851f7f0b360ed.jpg" 
@@ -24,7 +34,7 @@ export class ProductListComponent {
       "Modelo":"A4",
       "Descripcion":"4 puertas",
       "Precio": 510000,
-      "Year":2022,
+      "Year":"5 septiembre 2022",
       "Marca":"AUDI",
       "Color":"Blanco",
       "ImagenUrl":"https://acroadtrip.blob.core.windows.net/catalogo-imagenes/s/RT_V_7dddd9bd89814f5e9cbbbafdc08915ab.jpg" 
@@ -34,7 +44,7 @@ export class ProductListComponent {
       "Modelo":"Cheyenne",
       "Descripcion":"4 puertas",
       "Precio": 970000,
-      "Year":2024,
+      "Year":"4 julio 2024",
       "Marca":"CHEVROLET",
       "Color":"Azul",
       "ImagenUrl":"https://www.chevrolet.com.mx/content/dam/chevrolet/na/mx/es/index/pickups-and-trucks/2024-cheyenne/mov/01-images/cheyenne-zr2-v2.jpg?imwidth=960" 
